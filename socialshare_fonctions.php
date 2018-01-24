4255f0244b82050027c701cbecb5a0dd82a67d92
Mist. GraphX <arnaud.berard@mister-graphx.com>
2018-01-24 19:30:17 +0100

    + ajout de la balise renvoyant le markup d'une icone . + ajout sur la liste

--- socialshare_fonctions.php
+++ socialshare_fonctions.php
@@ -11,14 +11,51 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
+include_spip('inc/config');
+/* balise_SOCIAL_ICON_dist()
+
+Retourne le markup necesssaire a l'affichage de l'icone
+de partage svg#symbol ou span.icon
+
+on passe en parametre la clé du tableau issu de la pipeline
+
+
+*/
+function balise_SOCIAL_ICON_dist($p){
+	$social_network = interprete_argument_balise(1, $p);
+	$p->interdire_scripts = false;
+	$p->code = "calculer_balise_SOCIAL_ICON($social_network)";
+	return $p;
+}
+/* calculer_balise_SOCIAL_ICON()
+
+@param $network - clé dans la liste des reseaux sociaux
+@return $icon_markup
+*/
+function calculer_balise_SOCIAL_ICON($social_network){
+	$sharers = lister_partages();
+	$network = $sharers[$social_network];
+	$class = $network['icon_class'];
+
+	if(lire_config('socialshare/insert_svg_symbols')){
+		$markup = "<svg class=\"$class\"><use xlink:href=\"#$social_network\"></use></svg>";
+	}else{
+		$markup = "<span class=\"$class\" aria-hidden=\"true\"></span>";
+	}
+
+	return $markup;
+}
+
+
+
 /*
  * function lister_partages
- * 
+ *
  */
 
 function lister_partages() {
-	
-	
+
+
 	$default = array(
 		'facebook' => array(
 			'label'=> 'FaceBook',
@@ -41,16 +78,16 @@ function lister_partages() {
 			'icon_class'=> 's-icon s-icon-pinterest'
 		),
 	);
-	
+
 	if(is_array(pipeline('social_networks'))) {
 		$user_sharers= pipeline('social_networks');
-		
-		
+
+
 		$sharers = array_replace_recursive($default,$user_sharers['social_networks']);
-		
+
 		return $sharers;
 	}
-	
+
 	return $default;
-	
+
 }
